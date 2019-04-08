c75ecc778fc4b37a23f43832e4ae005285f4f2f9
Mahrukh2 <49372661+Mahrukh2@users.noreply.github.com>
2019-04-08 12:55:52 +0500

    Add files via upload

---
+++ OOP LAB No.1/task1.java
@@ -0,0 +1,18 @@
+public class task1{
+public static void main(String args[]){
+String name="Mahrukh Khan";
+String rollno = "18SW02";
+int totalmarks=100;
+float obtmarks=88;
+float per=(obtmarks/totalmarks)*100;
+char g='A';
+boolean status=true;
+System.out.println("Name:  " + name);
+System.out.println("Roll No. :  " + rollno);
+System.out.println("Total Marks:  " + totalmarks);
+System.out.println("Obtained Marks:  " + obtmarks);
+System.out.println("Percentage:  " + per);
+System.out.println("Grade:  " + g);
+System.out.println("Status: pass  " + status);
+}
+}
