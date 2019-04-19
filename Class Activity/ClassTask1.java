9df659afbe380ba8add33569f24b161adb337e1c
Mahrukh2 <49372661+Mahrukh2@users.noreply.github.com>
2019-04-19 11:48:06 +0500

    Add files via upload

---
+++ Class Activity/ClassTask1.java
@@ -0,0 +1,20 @@
+class ClassTask1{
+  public static void main(String args[]){
+	System.out.println("Even Numbers");
+      for(int i=0;i<=50;i++)
+       {
+        if(i%2==0)
+        {
+        System.out.println(i);
+        }
+      }
+      System.out.println("Odd Number");
+      for(int j=0;j<=50;j++)
+      {
+       if(j%2==1)
+	{
+	System.out.println(j);
+	 }
+      }
+  }
+}
---
+++ Class Activity/ClassTask3.java
@@ -0,0 +1,12 @@
+class ClassTask3{
+  public static void main(String args[]){
+    long n,fact=1;
+    n=Long.parseLong(args[0]);
+      for(int i=1;i<=n;i++)
+      {
+        fact=fact*i;
+
+      }
+      System.out.println("Factorial is :" + fact);
+   }
+ }
