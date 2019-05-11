8d4978dce6c5e25eb4a054df38a81b486afd950f
Mahrukh2 <49372661+Mahrukh2@users.noreply.github.com>
2019-05-11 16:15:04 +0500

    Add files via upload

---
+++ OOP LabNo.5/Lab5Task7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Lab5Task7{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+      String str=sc.nextLine();
+      String split[]=str.split(",");
+          System.out.println("Student 1");
+          System.out.println("Name : " + split[0]);
+          System.out.println("Age : " + split[1]);
+          System.out.println("Pogram : " + split[2]);
+          System.out.println("Student 2");
+          System.out.println("Name : " + split[3]);
+          System.out.println("Age : " + split[4]);
+          System.out.println("Program : " + split[5]);
+       }
+     }
